42808b92bfde007ee078e4416a1f8f7f708f7036
Юра <yurikss2005@gmail.com>
2017-01-29 23:26:26 +0300

    guessing-game/[feater]: i finished

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    class GuessingGame {
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.current = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.current = Math.ceil((this.min + this.max) / 2);
+        return this.current;
     }
 
     lower() {
-
+        this.max = this.current;
     }
 
     greater() {
-
+        this.min = this.current;
     }
 }
 
 module.exports = GuessingGame;
+
+
